0737e02ca0d05ec307f301d089ced248060f846b
theKianoush <keyonranjbar@aol.com>
2022-04-03 22:16:43 -0600

    "first commit"

--- server.js
+++ server.js
@@ -127,6 +127,6 @@ router.route('/movies')
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
-//module.exports = app; // for testing only
+module.exports = app; // for testing only
 
 
