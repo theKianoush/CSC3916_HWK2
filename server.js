48efd030e3c6b9a595e3f77816513c5f733f0d99
theKianoush <keyonranjbar@aol.com>
2022-04-04 22:00:00 -0600

    "mongo changes"

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ Description: Web API scaffolding for Movie API
  */
 
 var express = require('express');
-var http = require('http');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var authController = require('./auth');
 var authJwtController = require('./auth_jwt');
-db = require('./db')(); //hack
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
+var User = require('./Users');
 //require('dotenv').config();
 
 var app = express();
@@ -46,31 +45,48 @@ router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password
-        };
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        user.save(function(err){
+            if (err){
+                if(err.code == 11000)
+                    return res.json({success: false, message: 'A user with that username already exists. '});
+                else
+                    return res.json(err);
+
+            }
+
+            res.json({success: true, msg: 'Successfully created new user.'})
+        })
 
         db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successfully created new user.'})
+
     }
 });
 
 router.post('/signin', function (req, res) {
-    var user = db.findOne(req.body.username);
+    var userNew = new User();
+    userNew.username = req.body.username;
+    userNew.password = req.body.password;
 
-    if (!user) {
-        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-    } else {
-        if (req.body.password == user.password) {
-            var userToken = { id: user.id, username: user.username };
-            var token = jwt.sign(userToken, process.env.SECRET_KEY);
-            res.json ({success: true, token: 'JWT ' + token});
+    User.findOne({username: userNew.username}).select('name username password').exec(function (err, user) {
+        if (err) {
+            res.send(err);
         }
-        else {
-            res.status(401).send({success: false, msg: 'Authentication failed.'});
-        }
-    }
+
+        user.comparePassword(userNew.password, function (ismatch) {
+            if (isMatch) {
+                var userToken = {id: user.id, username: user.username};
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                res.json({success: true, token: 'JWT ' + token});
+            } else {
+                res.status(401).send({success: false, msg: 'Authentication failed.'});
+            }
+        })
+    })
 });
 
 
@@ -131,6 +147,6 @@ router.route('/movies')
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
-//module.exports = app; // for testing only
+module.exports = app; // for testing only
 
 
--- test/testcollection.js
+++ test/testcollection.js
@@ -3,7 +3,7 @@ require('dotenv').config({path:envPath});
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
-let db = require('../db')();
+let User = require('../Users');
 chai.should();
 
 chai.use(chaiHttp);
@@ -16,12 +16,17 @@ let login_details = {
 
 describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth', () => {
    beforeEach((done) => { //Before each test initialize the database to empty
-       db.userList = [];
+       //db.userList = [];
+
+
        done();
     })
 
     after((done) => { //after this test suite empty the database
-        db.userList = [];
+       // db.userList = [];
+        User.deleteOne({ name: 'test'}, function(err, user) {
+            if (err) throw err;
+        });
         done();
     })
 
@@ -32,6 +37,7 @@ describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth'
               .post('/signup')
               .send(login_details)
               .end((err, res) =>{
+                  console.log(JSON.stringify(res.body));
                 res.should.have.status(200);
                 res.body.success.should.be.eql(true);
                 //follow-up to get the JWT token
@@ -43,32 +49,13 @@ describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth'
                         res.body.should.have.property('token');
 
                         let token = res.body.token;
-                        //console.log('got token ' + token)
-                        //lets call a protected API
-                        chai.request(server)
-                            .put('/testcollection')
-                            .set('Authorization', token)
-                            .send({echo: ''})
-                            .end((err, res) => {
-                                res.should.have.status(200);
-                                res.body.body.should.have.property('echo');
-                                done();
-                            })
+                        console.log(token);
+                        done();
+
+
                     })
               })
         })
     });
 
-   describe('/testcollection fail auth', () => {
-      it('delete requires basic auth failed login', (done) => {
-          chai.request(server)
-              .delete('/testcollection')
-              .auth('cu_user', 'cu_rulez1')
-              .send({ echo: '' })
-              .end((err, res) => {
-                  res.should.have.status(401);
-                  done();
-              })
-      });
-   });
 });
