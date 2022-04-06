ff75c0d18b8e9120dfa4c9ef4f21dd097e4b61a1
theKianoush <keyonranjbar@aol.com>
2022-04-06 03:08:16 -0600

    "update 2.0"

--- server.js
+++ server.js
@@ -122,40 +122,40 @@ router.route('/movies')
         }
     })
 
-    // updating a movie
+
+
+    //Update movies
     .put(authJwtController.isAuthenticated, function(req, res) {
-        if(!req.body.title){
-            res.json({success:false, message: "Title is required."});
-        }else{      // data provided
-            if (req.body.newTitle){
-                Movie.findOneAndUpdate(req.body.title, req.body.newTitle, function(err, movie) {
-                    if(err){
-                        res.status(403).json({success:false, message: "Error: Could not make a change."});
-                    }else{
-                        res.status(200).json({success: true, message: "Title has been updated successfully"});
-                    }
-                });
-            }
-            if (req.body.newYear){
-                Movie.findOneAndUpdate(req.body.year, req.body.newYear, function(err, movie) {
-                    if(err){
-                        res.status(403).json({success:false, message: "Error: Could not make a change."});
-                    }else{
-                        res.status(200).json({success: true, message: "Released year has been updated successfully"});
-                    }
-                });
-            }
-            if (req.body.newGenre){
-                Movie.findOneAndUpdate(req.body.genre, req.body.newGenre, function(err, movie) {
-                    if(err){
-                        res.status(403).json({success:false, message: "Error: Could not make a change."});
-                    }else{
-                        res.status(200).json({success: true, message: "Genre has been updated successfully"});
-                    }
-                });
-            }
+        if (!req.body.title) {
+            res.json({success: false, msg: 'Please pass a Movie Title to update.'});
+        } else {
+            Movie.findOne({title: req.body.title}, function (err, movies) {
+                if (err) throw err;
+                else {
+                    //var movie = new Movie();
+                    movies.title = req.body.title;
+                    movies.yearReleased = req.body.yearReleased;
+                    movies.genre = req.body.genre;
+                    movies.actors = req.body.actors;
+                    movies.imageURL = req.body.imageURL;
+
+                    movies.save(function (err) {
+                        if (err) throw err;
+                        //else
+                        //console.log(movies);
+                        //res = res.status(200);
+                        res.json({success: true, msg: 'Movie successfully updated.'});
+                    })
+                }
+            })
         }
     })
+
+
+
+
+
+
 
     //Delete movies
     .delete(authJwtController.isAuthenticated, function(req, res) {
