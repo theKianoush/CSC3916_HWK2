8fdeee3c02c9308a0826513416638ea710e21813
theKianoush <keyonranjbar@aol.com>
2022-04-23 06:16:54 -0600

    x

--- server.js
+++ server.js
@@ -250,7 +250,7 @@ router.route('/movies/:movieId')
                              res.json(err);
                         }else{
 
-                             res.json(movie[0]);
+                             res.json(movie[0][0]);
                         }
                     })
                 }
