0fdf4758ce39becb8fb85b577882eb10ab71068e
theKianoush <keyonranjbar@aol.com>
2022-04-23 03:15:38 -0600

    derk

--- server.js
+++ server.js
@@ -222,24 +222,24 @@ router.route('/movies/:movieId')
 //GET = is supposed to get movie with parameter
 .get(authJwtController.isAuthenticated, function (req,res) {
     if (req.query && req.query.reviews && req.query.reviews === "true") {
-        Movie.findById(req.params.movieId, function (err, movies) {
+        Movie.findById(req.params.movieId, function (err, movie) {
             if (err)  throw err;
             else {
                 Movie.aggregate()
                     .lookup({from: 'reviews', localField: 'title', foreignField: 'title', as: 'reviews'})
-                    .exec(function (err, movies) {
+                    .exec(function (err, movie) {
                         if (err) {
                             res.status(500).send(err);
                         } else {
-                            res.json(movies);
+                            res.json(movie);
                         }
                     })
             }
         })
     } else {
-        Movie.findById(req.params.movieId, function (err, movies) {
+        Movie.findById(req.params.movieId, function (err, movie) {
             if (err)  throw err;
-            else { res.json(movies);}
+            else { res.json(movie);}
     })
     }
 });
