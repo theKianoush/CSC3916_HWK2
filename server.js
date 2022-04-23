42540704f27bb72cc6fe9bb79a92381f2b4ee049
theKianoush <keyonranjbar@aol.com>
2022-04-22 20:48:02 -0600

    newnew

--- server.js
+++ server.js
@@ -216,17 +216,69 @@ router.route('/movies/:movieId')
 
 
 
+
+    // if we get the query true
+    // then we append review to end of movie
+    // if not just find movie
+    // simple if else goin on here
+
     //GET = is supposed to get movie with parameter
-    .get(authJwtController.isAuthenticated, function (req,res) {
-        Movie.findById(req.params.movieId, function (err, movie)  {
+
+
+.get(authJwtController.isAuthenticated, function (req,res) {
+
+
+
+    // if query param is there and title of movie/review, it will find the review and append the movie to the end of it
+     if (req.query && req.query.reviews && req.query.reviews === "true") {
+
+         Movie.findById(req.params.movieId, function (err, movie) {
+             if (err) {
+                 res.send(err);
+                 console.log(err);
+             } else {
+                 Movie.aggregate([{
+                     $match: {"title": movie.title}
+                 },{
+                     $lookup: {
+                         from: "reviews",
+                         localField: "title",
+                         foreignField: "title",
+                         as: "reviews"
+                     }
+                 }
+                 ]).exec(function(err,review){
+                     if(err){
+                         return res.json(err);
+                     }else{
+
+                         return res.json({movie});
+                     }
+                 })
+             }
+
+
+
+         })
+
+
+    }
+
+
+    // if no query param, but title is given, it will just find the movie with no review
+    else {
+        Movie.findById(req.params.movieId, function (err, movie) {
             if (err) {
                 res.send(err);
                 console.log(err);
-            }
+            } else {
+                res.json(movie);
 
-            res.json(movie)
+            }
         })
-    });
+    }
+});
+
 
 
 
